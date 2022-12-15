2ed2acc92cc4c6d51f308c944463b70da16fdc05
moniquemichellemacias-y <moniquemichelle.macias-19@cpu.edu.ph>
2022-12-15 14:31:19 +0800

    MACIAS_JS

---
+++ js.js
@@ -0,0 +1,229 @@
+window.addEventListener("DOMContentLoaded", function (event) {
+    window.focus(); 
+  
+    
+    let snakePositions; 
+    let applePosition; 
+  
+    let startTimestamp; 
+    let lastTimestamp; 
+    let stepsTaken;
+    let score;
+    let contrast;
+  
+    let inputs; 
+  
+    let gameStarted = false;
+    let hardMode = false;
+  
+   
+    const width = 15; 
+    const height = 15; 
+    const speed = 200; 
+    let fadeSpeed = 5000; 
+    let fadeExponential = 1.024; 
+    const contrastIncrease = 0.5; 
+    const color = "#194569"; 
+  
+    const grid = document.querySelector(".grid");
+    for (let i = 0; i < width * height; i++) {
+      const content = document.createElement("div");
+      content.setAttribute("class", "content");
+      content.setAttribute("id", i); 
+  
+      const tile = document.createElement("div");
+      tile.setAttribute("class", "tile");
+      tile.appendChild(content);
+  
+      grid.appendChild(tile);
+    }
+  
+    const tiles = document.querySelectorAll(".grid .tile .content");
+  
+    const containerElement = document.querySelector(".container");
+    const noteElement = document.querySelector("footer");
+    const contrastElement = document.querySelector(".contrast");
+    const scoreElement = document.querySelector(".score");
+  
+    
+    resetGame();
+  
+    
+    function resetGame() {
+      snakePositions = [168, 169, 170, 171];
+      applePosition = 100; 
+  
+      startTimestamp = undefined;
+      lastTimestamp = undefined;
+      stepsTaken = -1;
+      score = 0;
+      contrast = 1;
+  
+      inputs = [];
+  
+      contrastElement.innerText = ${Math.floor(contrast * 100)}%;
+      scoreElement.innerText = hardMode ? H ${score} : score;
+  
+      for (const tile of tiles) setTile(tile);
+  
+      setTile(tiles[applePosition], {
+        "background-color": color,
+        "border-radius": "50%"
+      });
+  
+      for (const i of snakePositions.slice(1)) {
+        const snakePart = tiles[i];
+        snakePart.style.backgroundColor = color;
+  
+        if (i == snakePositions[snakePositions.length - 1])
+          snakePart.style.left = 0;
+        if (i == snakePositions[0]) snakePart.style.right = 0;
+      }
+    }
+  
+    window.addEventListener("keydown", function (event) {
+      if (
+        ![
+          "ArrowLeft",
+          "ArrowUp",
+          "ArrowRight",
+          "ArrowDown",
+          " ",
+          "H",
+          "h",
+          "E",
+          "e"
+        ].includes(event.key)
+      )
+        return;
+  
+      event.preventDefault();
+  
+      if (event.key == " ") {
+        resetGame();
+        startGame();
+        return;
+      }
+  
+      if (event.key == "H" || event.key == "h") {
+        hardMode = true;
+        fadeSpeed = 4000;
+        fadeExponential = 1.025;
+        noteElement.innerHTML = Hard mode. Press space to start!;
+        noteElement.style.opacity = 1;
+        resetGame();
+        return;
+      }
+  
+      if (event.key == "E" || event.key == "e") {
+        hardMode = false;
+        fadeSpeed = 5000;
+        fadeExponential = 1.024;
+        noteElement.innerHTML = Easy mode. Press space to start!;
+        noteElement.style.opacity = 1;
+        resetGame();
+        return;
+      }
+  
+      if (
+        event.key == "ArrowLeft" &&
+        inputs[inputs.length - 1] != "left" &&
+        headDirection() != "right"
+      ) {
+        inputs.push("left");
+        if (!gameStarted) startGame();
+        return;
+      }
+      if (
+        event.key == "ArrowUp" &&
+        inputs[inputs.length - 1] != "up" &&
+        headDirection() != "down"
+      ) {
+        inputs.push("up");
+        if (!gameStarted) startGame();
+        return;
+      }
+      if (
+        event.key == "ArrowRight" &&
+        inputs[inputs.length - 1] != "right" &&
+        headDirection() != "left"
+      ) {
+        inputs.push("right");
+        if (!gameStarted) startGame();
+        return;
+      }
+      if (
+        event.key == "ArrowDown" &&
+        inputs[inputs.length - 1] != "down" &&
+        headDirection() != "up"
+      ) {
+        inputs.push("down");
+        if (!gameStarted) startGame();
+        return;
+      }
+    });
+  
+    function startGame() {
+      gameStarted = true;
+      noteElement.style.opacity = 0;
+      window.requestAnimationFrame(main);
+    }
+  
+    function main(timestamp) {
+      try {
+        if (startTimestamp === undefined) startTimestamp = timestamp;
+        const totalElapsedTime = timestamp - startTimestamp;
+        const timeElapsedSinceLastCall = timestamp - lastTimestamp;
+  
+        const stepsShouldHaveTaken = Math.floor(totalElapsedTime / speed);
+        const percentageOfStep = (totalElapsedTime % speed) / speed;
+  
+        if (stepsTaken != stepsShouldHaveTaken) {
+          stepAndTransition(percentageOfStep);
+  
+          const headPosition = snakePositions[snakePositions.length - 1];
+          if (headPosition == applePosition) {
+            score++;
+            scoreElement.innerText = hardMode ? H ${score} : score;
+  
+            addNewApple();
+  
+            contrast = Math.min(1, contrast + contrastIncrease);
+  
+            console.log(Contrast increased by ${contrastIncrease * 100}%);
+            console.log(
+              "New fade speed (from 100% to 0% in milliseconds)",
+              Math.pow(fadeExponential, score) * fadeSpeed
+            );
+          }
+  
+          stepsTaken++;
+        } else {
+          transition(percentageOfStep);
+        }
+  
+        if (lastTimestamp) {
+          const contrastDecrease =
+            timeElapsedSinceLastCall /
+            (Math.pow(fadeExponential, score) * fadeSpeed);
+          contrast = Math.max(0, contrast - contrastDecrease);
+        }
+  
+        contrastElement.innerText = ${Math.floor(contrast * 100)}%;
+        containerElement.style.opacity = contrast;
+  
+        window.requestAnimationFrame(main);
+      } catch (error) {
+        const pressSpaceToStart = "Press space to reset the game.";
+        const changeMode = hardMode
+          ? "Back to easy mode? Press the letter E."
+          : "Ready for hard more? Press the letter H.";
+        
+        noteElement.innerHTML = ${error.message}. ${pressSpaceToStart} <div>${changeMode}</div>;
+        noteElement.style.opacity = 1;
+        containerElement.style.opacity = 1;
+      }
+  
+      lastTimestamp = timestamp;
+    }
+  
